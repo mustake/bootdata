b82efbfc89ddb024f1fe0ce131723a4cbca1557a
mu_stake <623087739@qq.com>
2018-01-31 11:16:25 +0800

    new  spring boot

---
+++ src/main/java/com/djb/bootdata/BootdataApplication.java
@@ -0,0 +1,12 @@
+package com.djb.bootdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdataApplication.class, args);
+	}
+}
